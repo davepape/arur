95003546cd12a0611551401ca906a81651c25d5c
Dave Pape <depape@buffalo.edu>
2022-07-27 07:22:38 -0700

    backup websockets version

---
+++ ws/app-ws.js
@@ -0,0 +1,106 @@
+const express = require('express');
+let app = express();
+
+app.use(function (req,res,next) {
+    res.set('Cache-Control','no-store');
+    next();
+    });
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const fs = require('fs');
+let fulltext = fs.readFileSync('RUR.txt', 'utf-8');
+let lines = fulltext.split("\n");
+let curLine = 1;
+
+let userData = { text: lines[0] }
+ 
+function getData(req, res) {
+    let sendData = {};
+    sendData.text = userData.text.substr(0,userData.text.indexOf('['))
+    let str = JSON.stringify(sendData);
+    res.send(str);
+    }
+
+app.get('/getdata', getData);
+
+
+if (typeof(PhusionPassenger) != 'undefined') {
+    PhusionPassenger.configure({ autoInstall: false });
+    let server = app.listen('passenger', function () {});
+    }
+else {
+    let server = app.listen(8000, function () { console.log('non-passenger server started');});
+    }
+
+
+
+const { WebSocket, WebSocketServer } = require('ws');
+
+const httpserver = require('http').createServer();
+httpserver.listen(8081);
+
+const wsServer = new WebSocketServer({server: httpserver}, function () { console.log('wsServer started'); });
+
+wsServer.on('connection', newConnection);
+
+function newConnection(ws)
+    {
+    ws.on('message', function (data) { receiveData(data,ws); });
+    ws.send(userData.text, { binary: false});
+    }
+
+let lastCue = lines[0];
+
+function receiveData(data,ws)
+    {
+    userData.text = data.toString();
+    if (userData.text == lines[curLine])
+        {
+        lastCue = userData.text;
+        curLine = (curLine+1) % lines.length;
+        if (curLine == 0)
+            {
+            console.log(new Date());
+            lastCue = lines[0];
+            curLine = 1;
+            }
+        if (curLine % 100 == 0)
+            console.log(`line ${curLine}`);
+        }
+    broadcast(data, ws);
+    }
+
+function broadcast(data, ws)
+    {
+    wsServer.clients.forEach(function (client) {
+        if ((client != ws) && (client.readyState === WebSocket.OPEN)) {
+            client.send(data, { binary: false });
+            }
+        });
+    }
+
+setInterval(prompter, 100);
+
+let lastCheckedLine = curLine;
+let missedCount = 0;
+
+function prompter()
+    {
+    if (curLine != lastCheckedLine)
+        {
+        lastCheckedLine = curLine;
+        missedCount = 0;
+        }
+    else
+        {
+        missedCount++;
+        if (missedCount >= 10)
+            {
+            console.log(`prompting ${lastCue}`);
+            broadcast(lastCue, 0);
+            missedCount = 0;
+            }
+        }
+    }
---
+++ ws/readothers-ws.js
@@ -0,0 +1,96 @@
+const WebSocket = require('ws')
+const url = 'ws://arur.site:8081'
+const ws = new WebSocket(url)
+
+ws.onopen = () => {
+  console.log('open');
+}
+
+ws.onerror = (error) => {
+  console.log(`WebSocket error:`); console.log(JSON.stringify(error));
+}
+
+ws.onmessage = receiveMessage;
+
+
+const fs = require('fs');
+
+let otherName = 'DOMIN';
+
+let fulltext = fs.readFileSync('RUR.txt', 'utf-8');
+let lines = fulltext.split("\n");
+let myLines = [];
+let startingLine = true;
+let newLineInfo;
+let lastCue = lines[0];
+for (let i=1; i < lines.length; i++)
+    {
+    if (!(lines[i].startsWith(otherName+'.')))
+        {
+        if (startingLine)
+            {
+            newLineInfo = { cue: lastCue, lines: [] };
+            myLines.push(newLineInfo);
+            startingLine = false;
+            }
+        newLineInfo.lines.push(lines[i]);
+        }
+    else
+        {
+        lastCue = lines[i];
+        startingLine = true;
+        }
+    }
+
+let curLine = 0, curSubLine = 0;
+let sayingLine = false;
+
+setInterval(sayLine, 1000);
+
+
+function receiveMessage(msg)
+    {
+    let text = msg.data;
+    debugMessage(`heard ${text}`);
+    if (!sayingLine)
+        {
+        if (text == myLines[curLine].cue)
+            {
+            sayingLine = true;
+            debugMessage(`that's my cue!`);
+            }
+        }
+    }
+
+let saidWaiting = false;
+
+function sayLine()
+    {
+    if (sayingLine)
+        {
+        debugMessage(`saying ${myLines[curLine].lines[curSubLine]}`);
+        ws.send(myLines[curLine].lines[curSubLine]);
+        curSubLine++;
+        if (curSubLine == myLines[curLine].lines.length)
+            {
+            curLine = (curLine+1) % myLines.length;
+            curSubLine = 0;
+            sayingLine = false;
+            }
+        saidWaiting = false;
+        }
+    else
+        {
+        if (!saidWaiting)
+            {
+            debugMessage(`waiting for ${myLines[curLine].cue}`);
+            saidWaiting = true;
+            }
+        }
+    }
+
+
+function debugMessage(str)
+    {
+    console.log(str);
+    }
